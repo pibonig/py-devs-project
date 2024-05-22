bb96840ed472fde1c73ec6282eaaa45de0bff986
Vasyl Vintoniv <126397140+vinvas94@users.noreply.github.com>
2024-05-22 23:36:15 +0200

    addnotecommand

--- src/assistant/commands/note/delete_note_command.py
+++ src/assistant/commands/note/delete_note_command.py
@@ -1,7 +1,18 @@
 from src.decorators import input_error
-from src.models.contact_book.contact_book import ContactBook
+from src.models.notebook.notebook import NoteBook
 
 
 @input_error
-def delete_note_command(args: list, contact_book: ContactBook):
-    pass
+def delete_note_command(args: list, notebook: NoteBook):
+    if len(args)!=1:
+        raise ValueError("Invalid arguments. Example: delete_note <note>")
+    try:
+        note_index=int(args[0])
+    except ValueError:
+        raise "Note index must be an integer."
+    deleted_note= notebook.delete_note(note_index)
+    if deleted_note:
+        return f"Note deleted: {deleted_note}"
+    else:
+        return "Note not found"
+
--- src/assistant/commands/note/get_all_notes_command.py
+++ src/assistant/commands/note/get_all_notes_command.py
@@ -1,7 +1,13 @@
 from src.decorators import input_error
-from src.models.contact_book.contact_book import ContactBook
+from src.models.notebook.notebook import NoteBook
 
 
 @input_error
-def get_all_notes_command(contact_book: ContactBook):
-    pass
+def get_all_notes_command(args: list, notebook: NoteBook):
+    notes=notebook.list_notes()
+    if isinstance(notes,str):
+        return notes
+    elif not notes:
+        return "No notes available"
+    else:
+        return "\n".join(str(note) for note in notes)
