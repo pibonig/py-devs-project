d9b490bd74769f2c3552ba8aad6cf8878eef6979
Ruslan <ruslan.grudistov@gmail.com>
2024-05-23 17:51:38 +0300

    1

--- src/assistant/commands/address/add_address_command.py
+++ src/assistant/commands/address/add_address_command.py
@@ -5,9 +5,9 @@ from src.models.contact_book.contact_book import ContactBook
 @input_error
 def add_address_command(args: list, contact_book: ContactBook):
     name, adress = args
-    record = contact_book.get_contact(name)
-    if record is None:
+    contact = contact_book.get_contact(name)
+    if contact is None:
         return 'Condact does not exist'
     else:
-        record.set_address(adress)
+        contact.set_address(adress)
         return 'Adress has been added to the contact'
