46f19cf9be57eb33a8027a2da92adc4acfe90366
Valerii Pometun <112505741+Valerii2022@users.noreply.github.com>
2024-05-25 15:16:57 +0300

    edit colorama

--- src/exceptions/invalid_command_params_exception.py
+++ src/exceptions/invalid_command_params_exception.py
@@ -2,4 +2,4 @@ from colorama import Fore, Style, Back
 
 class InvalidCommandParamsException(Exception):
     def __init__(self, cls):
-        super().__init__(Fore.RED + f"Invalid command parameters. {Back.WHITE}{Fore.GREEN}Example: {cls.name} {cls.signature}{Style.RESET_ALL}")
+        super().__init__(Fore.RED + f"Invalid command parameters. {Back.WHITE}{Fore.GREEN}Example: {cls.name} {cls.signature}.")
