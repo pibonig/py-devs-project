1ede9af17b884d54c54d1fcdff06c350b9ad8f21
Valerii Pometun <112505741+Valerii2022@users.noreply.github.com>
2024-05-26 16:10:33 +0300

    update birthdays command

--- src/assistant/commands/birthday/get_all_birthdays_command.py
+++ src/assistant/commands/birthday/get_all_birthdays_command.py
@@ -1,23 +1,22 @@
 from src.decorators import input_error
 from src.exceptions.invalid_command_params_exception import InvalidCommandParamsException
 from src.models.contact_book.contact_book import ContactBook
-
+from colorama import Fore
 
 class GetAllBirthdaysCommand:
     name = "get_all_birthdays"
-    signature = "<number of days>"
-    description = "Show all contacts whose birthdays are a specified number of days from the current date."
+    signature = ""
+    description = "Show birthdays for all contacts."
 
     @input_error
     def execute(self, args: list, contact_book: ContactBook):
-        if len(args) != 1:
+        if len(args) != 0:
             raise InvalidCommandParamsException(self)
         
         if not contact_book.data:
             return "Address book is empty."
         else:
-            days = int(args[0])
-            result = contact_book.get_upcoming_birthdays(days)
+            result = contact_book.get_upcoming_birthdays(None)
             if result is None:
-                return f"No contacts have birthdays within the next {days} days."
+                return Fore.RED + f"Birthdays not provided for any contacts."
             return result
